db609a565b92dae591b2988c0b740af063070984
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2024-02-05 13:01:00 +0530

    Worked on the spreadsheet project

--- SpreadSheet/script.js
+++ SpreadSheet/script.js
@@ -1,6 +1,16 @@
 const infixToFunction = {
-  
+  "+": (x, y) => x + y,
+  "-": (x, y) => x - y,
+  "*": (x, y) => x * y,
+  "/": (x, y) => x / y,
+}
 
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
 }
 
 const isEven = num => num % 2 === 0;
@@ -20,6 +30,13 @@ const spreadsheetFunctions = {
   sum,
   average,
   median
+}
+
+const applyFunction = str => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
