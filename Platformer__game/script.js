1372e84c8f53cc8dc0ec81d8581cba75ab2535b0
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2024-02-15 19:20:35 +0530

    Worked on the platformer game

--- Platformer__game/script.js
+++ Platformer__game/script.js
@@ -83,6 +83,16 @@ const movePlayer = (key, xVelocity, isPressed) => {
   }
 
 
+  switch (key) {
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+    
+  }
 
 }
 
