fafacda6ff9dc105ecfb5d69226817fe47b96a12
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2024-02-24 17:29:18 +0530

    Completed the quiz app

---
+++ QuizApp/script.js
@@ -0,0 +1,121 @@
+const questions = [
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Giraffe", correct: false },
+      { text: "Elephant", correct: false },
+    ],
+  },
+  {
+    question:
+      "A flashing red traffic light signifies that a driver should do what?",
+    answers: [
+      { text: "Speed up", correct: false },
+      { text: "stop", correct: true },
+      { text: "proceed with caution", correct: false },
+      { text: "honk the horn", correct: false },
+    ],
+  },
+  {
+    question: "A knish is traditionally stuffed with what filling?",
+    answers: [
+      { text: "creamed corn", correct: false },
+      { text: "potato", correct: true },
+      { text: "lemon custard", correct: false },
+      { text: "rasberry jelly", correct: false },
+    ],
+  },
+  {
+    question: "According to Greek mythology, who was Apollo's twin sister?",
+    answers: [
+      { text: "Aphrodite", correct: false },
+      { text: "Artemis", correct: true },
+      { text: "Venus", correct: false },
+      { text: "Athena", correct: false },
+    ],
+  },
+];
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const startQuiz = () => {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+};
+
+const showQuestion = () => {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+};
+
+const resetState = () => {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+};
+
+const selectAnswer = (e) => {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("Correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("Incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("Correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+};
+
+const showScore = () =>{
+  resetState()
+  questionElement.innerHTML = `You Scored ${score} out of ${questions.length} !`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display ="block";
+}
+
+const handleNextButton = () => {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
